69080b4b8214e995e5d63f7599711589b6f0fc9d
Islam Elsayed <107177143+Liftingthedata@users.noreply.github.com>
2022-12-20 21:44:47 +0200

    Update get_bento.py

--- Capstone Project - 1/get_bento.py
+++ Capstone Project - 1/get_bento.py
@@ -1,6 +1,6 @@
 import bentoml
 from tensorflow import keras
 
-model = keras.models.load_model("xception_only_lr.h5")
+model = keras.models.load_model("model/xception_only_lr.h5")
 bentoml.keras.save_model("kitchenware-classifier", model)
 
