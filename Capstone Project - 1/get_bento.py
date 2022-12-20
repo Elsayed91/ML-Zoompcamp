9493cbd04b893ef517cd42c230f97b7ba74cfee4
Islam Elsayed <107177143+Liftingthedata@users.noreply.github.com>
2022-12-20 21:35:16 +0200

    Update get_bento.py

--- Capstone Project - 1/get_bento.py
+++ Capstone Project - 1/get_bento.py
@@ -1,9 +1,6 @@
-import os
-
 import bentoml
-import numpy as np
-import tensorflow as tf
 from tensorflow import keras
 
-model = keras.models.load_model('model/EfficientNetB401_0.962.h5')
-bentoml.keras.save_model("keras_effnet", model)
+model = keras.models.load_model("xception_only_lr.h5")
+bentoml.keras.save_model("kitchenware-classifier", model)
+
