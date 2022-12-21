a090843cf68e0768b59eaeebb147c8193ea61447
Islam Elsayed <107177143+Liftingthedata@users.noreply.github.com>
2022-12-21 02:39:52 +0200

    Update train.py

--- Capstone Project - 1/train.py
+++ Capstone Project - 1/train.py
@@ -1,22 +1,20 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import seaborn as sns
+
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.efficientnet import EfficientNetB4
-from tensorflow.keras.applications.efficientnet import preprocess_input as preprocess_input_efficient
+from tensorflow.keras.applications.xception import Xception
+from tensorflow.keras.applications.xception import preprocess_input as preprocess_input_xcep
+import bentoml
 
 
 
-
-def load_data(csv_file):
+def load_data(csv_file, path='data/images/'):
   train = pd.read_csv(csv_file)
-  train['filename'] = '/kaggle/input/kitchenware-classification/images/' + train['Id'].map(lambda x: f'{x:0>4}.jpg').astype(str)
+  train['filename'] = path + train['Id'].map(lambda x: f'{x:0>4}.jpg').astype(str)
   return train
 
 def split_data(data, cuttoff_percentage=0.8): 
@@ -25,11 +23,11 @@ def split_data(data, cuttoff_percentage=0.8):
   df_val = train[cutoff:]
   return df_train, df_val
 
-def data_generator(preprocessing_function, df, y_col='label', class_mode='categorical') 
-  data_generator = ImageDataGenerator(preprocessing_function=preprocess_input_efficient)   
+def data_generator(preprocessing_function, df, y_col='label', class_mode='categorical', path='data/images/') 
+  data_generator = ImageDataGenerator(preprocessing_function=preprocessing_function)   
   df_generator = data_generator.flow_from_dataframe(
           dataframe=df,
-          directory='/kaggle/input/kitchenware-classification/images/',
+          directory=path,
           x_col='filename',
           y_col=y_col,
           target_size=(300, 300),
@@ -38,12 +36,12 @@ def data_generator(preprocessing_function, df, y_col='label', class_mode='catego
       )
   return df_generator
 
-def train_efficient_net():
-  base_model = EfficientNetB4(
-      weights='imagenet',
-      include_top=False,
-      input_shape=(300, 300, 3)
-  )
+def train_model():
+  base_model = Xception(
+    weights='imagenet',
+    include_top=False,
+    input_shape=(299, 299, 3)
+)
   base_model.trainable = False
   inputs = keras.Input(shape=(300, 300, 3))
   base = base_model(inputs, training=False)
@@ -58,23 +56,22 @@ def train_efficient_net():
       metrics=['accuracy']
   )
   history = model.fit(train_generator, epochs=5, validation_data=validation_generator)
-  return model, history
+  return model
 
-def predict(train_generator, test_generator):
-  y_pred = model.predict(test_generator)
-  classes = np.array(list(train_generator.class_indices.keys()))
-  return classes[y_pred.argmax(axis=1)]
 
-def main(training_file, test_file):
-  training_df = load_data(training_file)
-  test_df = load_data(test_file)
+
+def main(train_csv, test_csv):
+  df_train_full= load_data(train_csv)
   df_train, df_val = split_data(training_df)
-  training_generator = data_generator(preprocess_input_efficient, df_train, y_col='label', class_mode='categorical')
-  validation_generator = data_generator(preprocess_input_efficient, df_val, y_col='label', class_mode='categorical')
-  test_generator = data_generator(preprocess_input_efficient, df_test, y_col=None, class_mode=None)
-  predictions = predict(train_generator, test_generator)
+  training_generator = data_generator(preprocess_input_xcep, df_train, y_col='label', class_mode='categorical')
+  validation_generator = data_generator(preprocess_input_xcep, df_val, y_col='label', class_mode='categorical')
+  model = train_model()
+  save_model = bentoml.keras.save_model("kitchenware-classifier",
+                                       model)
 
   
 
 if __name__ == '__main__':
-  main(training_file, test_file)
+  train_csv = os.getenv('train_csv')
+  test_csv = os.getenv('test_csv')
+  main(train_csv, test_csv)
